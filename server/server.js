a1f33d16ae2104063b3c1489ef503b29b33f38cc
Astha Kesharwani <kesharwaniastha9@gmail.com>
2025-11-06 16:02:20 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const app = express()
 
 await connectCloudinary()
 
-app.use(cors())
+app.use(cors({
+    origin: [process.env.CLIENT_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }));
 app.use(express.json())
 app.use(clerkMiddleware())
 
